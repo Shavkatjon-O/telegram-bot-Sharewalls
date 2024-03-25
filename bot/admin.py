7275432a87d34112314bb9a51d614be9d28f009d
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-25 15:14:37 +0500

    feat: copy original models

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.FileMessage)
+admin.site.register(models.MediaGroupItem)
+admin.site.register(models.Admin)
+admin.site.register(models.TGusers)
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class FileMessage(models.Model):
+    file_id = models.CharField(max_length=255, null=True, blank=True)
+    url = models.CharField(max_length=9, unique=True, null=True, blank=True)
+    forward_message_id = models.IntegerField(null=True, blank=True)
+
+    downloads = models.IntegerField(default=0)
+    unique_downloads = models.IntegerField(default=0)
+
+    downloads_second = models.IntegerField(default=0)
+    unique_downloads_second = models.IntegerField(default=0)
+
+    is_media_group = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Temp FileMessage"
+        verbose_name_plural = "Temp FileMessages"
+
+
+class MediaGroupItem(models.Model):
+    file_message = models.ForeignKey(FileMessage, on_delete=models.CASCADE)
+
+    file_id = models.CharField(max_length=255)
+    file_content_type = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Temp MediaGroupItem"
+        verbose_name_plural = "Temp MediaGroupItems"
+
+
+class Admin(models.Model):
+    user_id = models.BigIntegerField(unique=True)
+
+    class Meta:
+        verbose_name = "Temp Admin"
+        verbose_name_plural = "Temp Admins"
+
+
+class TGusers(models.Model):
+    user_id = models.TextField()
+    language = models.TextField()
+    status = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Temp TGusers"
+        verbose_name_plural = "Temp TGusers"
