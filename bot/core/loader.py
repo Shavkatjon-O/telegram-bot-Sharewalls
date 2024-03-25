b90f086ebb21b36a5863175d433109d38cbc79aa
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-25 17:11:34 +0500

    fix: parse mode

--- bot/core/loader.py
+++ bot/core/loader.py
@@ -1,10 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.enums import ParseMode
 
 from django.conf import settings
 
 token = settings.TELEGRAM_BOT_TOKEN
 
-bot = Bot(token=token)
+bot = Bot(token=token, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
