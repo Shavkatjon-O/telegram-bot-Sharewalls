e81aa91de7785df9b45f03b4edba95da0060f5f3
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-03-25 14:31:44 +0500

    Delete users directory

--- users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "is_staff", "is_active", "created_at")
-    list_display_links = ("username",)
