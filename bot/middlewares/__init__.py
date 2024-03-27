9a71194d661a291253b5e887a251943a71dc8558
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-28 00:32:57 +0500

    feat: album middleware

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -3,5 +3,7 @@ from aiogram import Dispatcher
 
 def register_middlewares(dp: Dispatcher) -> None:
     from .auth import AuthMiddleware
+    from .album import AlbumMiddleware
 
     dp.message.middleware(AuthMiddleware())
+    dp.message.middleware(AlbumMiddleware())
