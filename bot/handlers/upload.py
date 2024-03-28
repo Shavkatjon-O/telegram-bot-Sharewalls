296e7bbf4df80b19b24266fbce0454a14ff043e4
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-28 12:13:29 +0500

    fix: upload states

--- bot/handlers/upload.py
+++ bot/handlers/upload.py
@@ -9,27 +9,19 @@ from bot.states import UploadStates
 from bot.models import TelegramUser
 
 
+"""
+
+1. User sends /upload command
+2. Bot asks user to send an image
+3. User sends an image
+4. Bot sends the image to user and asks for confirmation
+5. User sends confirmation
+6. Bot asks to add another one or finish
+7. User sends another one or finish
+8. User sends /finish command
+9. Bot saves images to database
+
+"""
+
+
 router = Router(name="upload")
-
-
-@router.message(Command("upload"), AdminFilter())
-async def command_upload_handler(message: Message, current_user: TelegramUser, state: FSMContext) -> None:
-    await state.set_state(UploadStates.UPLOAD_IMAGE)
-
-    await message.answer("Send me an image")
-
-
-@router.message(UploadStates.UPLOAD_IMAGE, F.content_type.in_([ContentType.PHOTO, ContentType.DOCUMENT]))
-async def process_upload_image(
-    message: Message, album: list[Message], current_user: TelegramUser, state: FSMContext
-) -> None:
-
-    media_group = []
-    for _message in album:
-        if _message.photo:
-            media_group.append(InputMediaPhoto(media=_message.photo[-1].file_id))
-        else:
-            media_group.append(InputMediaDocument(media=_message.document.file_id))
-
-    await message.answer_media_group(media=media_group)
-    await message.answer("Image received")
--- bot/states/upload.py
+++ bot/states/upload.py
@@ -4,3 +4,4 @@ from aiogram.fsm.state import StatesGroup, State
 class UploadStates(StatesGroup):
     UPLOAD_IMAGE = State()
     CONFIRMATION = State()
+    FINISH = State()
