818fcabf9bda54dc0f30530aba6937385ea60902
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-29 15:30:33 +0500

    feat: i18n middleware

--- bot/core/loader.py
+++ bot/core/loader.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.enums import ParseMode
+from aiogram.utils.i18n.core import I18n
 
 from django.conf import settings
 
@@ -9,3 +10,5 @@ token = settings.TELEGRAM_BOT_TOKEN
 bot = Bot(token=token, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
+
+i18n: I18n = I18n(path=settings.BASE_DIR / "locales", default_locale="en", domain="django")
--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,9 +1,20 @@
 from aiogram import Dispatcher
+from aiogram.utils.callback_answer import CallbackAnswerMiddleware
+
+from bot.core.loader import i18n as _i18n
 
 
 def register_middlewares(dp: Dispatcher) -> None:
+
     from .auth import AuthMiddleware
     from .album import AlbumMiddleware
+    from .i18n import ACLMiddleware
 
     dp.message.middleware(AuthMiddleware())
     dp.message.middleware(AlbumMiddleware())
+
+    dp.message.middleware(ACLMiddleware(i18n=_i18n))
+    dp.callback_query.middleware(ACLMiddleware(i18n=_i18n))
+    dp.inline_query.middleware(ACLMiddleware(i18n=_i18n))
+
+    dp.callback_query.middleware(CallbackAnswerMiddleware())
