21ad30e1301194c658575fe1ed2e0fb99d7dc067
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-03-29 14:13:17 +0500

    fix: formatter line length to 100

--- bot/handlers/upload.py
+++ bot/handlers/upload.py
@@ -13,30 +13,42 @@ router = Router(name="upload")
 
 
 @router.message(Command(commands=["upload"]), AdminFilter())
-async def command_upload_handler(message: Message, tg_user: TelegramUser, state: FSMContext) -> None:
+async def command_upload_handler(
+    message: Message, tg_user: TelegramUser, state: FSMContext
+) -> None:
     """Handler for uploading images to database by admin users only."""
 
     await state.set_state(UploadStates.UPLOAD_IMAGE)
 
 
-@router.message(UploadStates.UPLOAD_IMAGE, F.content_type.in_([ContentType.PHOTO, ContentType.DOCUMENT]))
-async def process_image(message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext) -> None:
+@router.message(
+    UploadStates.UPLOAD_IMAGE, F.content_type.in_([ContentType.PHOTO, ContentType.DOCUMENT])
+)
+async def process_image(
+    message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext
+) -> None:
     """Handler for uploading images to database by admin users only."""
 
     await state.set_state(UploadStates.CONFIRMATION)
 
 
 @router.message(UploadStates.CONFIRMATION, F.text == reply.ConfirmUploadChoices.ADD_IMAGE)
-async def process_add_image(message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext) -> None:
+async def process_add_image(
+    message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext
+) -> None:
 
     await state.set_state(UploadStates.UPLOAD_IMAGE)
 
 
 @router.message(UploadStates.CONFIRMATION, F.text == reply.ConfirmUploadChoices.FINISH)
-async def process_finish(message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext) -> None:
+async def process_finish(
+    message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext
+) -> None:
     pass
 
 
 @router.message(UploadStates.CONFIRMATION, F.text == reply.ConfirmUploadChoices.CANCEL)
-async def process_cancel(message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext) -> None:
+async def process_cancel(
+    message: Message, album: list[Message], tg_user: TelegramUser, state: FSMContext
+) -> None:
     pass
